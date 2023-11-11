b5f5a868b9291de50927baf19ae3b4c8cd2a7a10
EdwardHTEvans <edwardhtevans@protonmail.com>
2023-11-11 22:00:29 +0000

    added player tracking camera added camera panning

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,13 @@
+use std::time::Duration;
+
+use crate::player::Player;
 use bevy::prelude::*;
 use bevy::time::Stopwatch;
-use crate::player::Player;
-
 
 pub struct CameraManagerPlugin;
-fn setup_camera_manager(mut commands: Commands)
-{
-    commands.spawn(CameraManager{
-        time : Stopwatch::new()
+fn setup_camera_manager(mut commands: Commands) {
+    commands.spawn(CameraManager {
+        time: Stopwatch::new(),
     });
 }
 
@@ -18,38 +18,59 @@ impl Plugin for CameraManagerPlugin {
 }
 
 #[derive(Component)]
-struct CameraManager {
-    pub 
+pub struct CameraManager {
     time: Stopwatch,
 }
 
+// TODO add pan speeds, rates, and distance etc... to config?
 pub fn update_camera(
-    // input: Res<Input<KeyCode>>,
-    mut q_camera: Query<&mut Transform, (With<Camera>, Without<Player>)>,
-    q_player: Query<&Transform,  (With<Player>, Without<Camera>)>,
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+    mut q_camera_transform: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+    mut q_camera_manager: Query<&mut CameraManager>,
+    q_player_transform: Query<&Transform, (With<Player>, Without<Camera>)>,
 ) {
-    let mut camera_transform = match q_camera.get_single_mut() {
+    let mut camera_transform = match q_camera_transform.get_single_mut() {
         Ok(camera_transform) => camera_transform,
         Err(_) => return,
     };
 
-    let player_transform = match q_player.get_single() {
-        Ok(player_transform) => player_transform,
+    let mut camera_manager = match q_camera_manager.get_single_mut() {
+        Ok(camera_manager) => camera_manager,
         Err(_) => return,
     };
 
-    // if input.pressed(KeyCode::Right) {
-    //     camera_transform.translation += Vec3::new(2.0, 0.0, 0.0);
-    // };
-    // if input.pressed(KeyCode::Left) {
-    //     camera_transform.translation += Vec3::new(-2.0, 0.0, 0.0);
-    // };
-    // if input.pressed(KeyCode::Up) {
-    //     camera_transform.translation += Vec3::new(0.0, 2.0, 0.0);
-    // };
-    // if input.pressed(KeyCode::Down) {
-    //     camera_transform.translation += Vec3::new(0.0, -2.0, 0.0);
-    // };
+    let player_transform = match q_player_transform.get_single() {
+        Ok(player_transform) => player_transform,
+        Err(_) => return,
+    };
+    camera_manager.time.tick(time.delta());
 
-    camera_transform.translation = player_transform.translation;
+    let mut delta_transform = Vec3::new(0.0, 0.0, 0.0);
+    if input.pressed(KeyCode::Right) {
+        delta_transform.x += 1.0;
+        camera_manager.time.reset();
+    };
+    if input.pressed(KeyCode::Left) {
+        delta_transform.x -= 1.0;
+        camera_manager.time.reset()
+    };
+    if input.pressed(KeyCode::Up) {
+        delta_transform.y += 1.0;
+        camera_manager.time.reset()
+    };
+    if input.pressed(KeyCode::Down) {
+        delta_transform.y -= 1.0;
+        camera_manager.time.reset()
+    };
+
+    let delta_distance = camera_transform.translation - player_transform.translation;
+    let new_pos = camera_transform.translation + delta_transform;
+    if (new_pos - player_transform.translation).length() < 32.0 {
+        camera_transform.translation += delta_transform * (1.0 - (delta_distance.length() / 32.0));
+    }
+
+    if camera_manager.time.elapsed_secs() > 1.0 {
+        camera_transform.translation -= delta_distance * 0.025;
+    };
 }
