7d4441125782231e77c713346a6f18499b226131
Dan W <60625311+DWaterhouse1@users.noreply.github.com>
2023-11-11 11:43:40 +0000

    Added browser panic hook.
    When running the game in a browser, panics are lost. This enables a browser
    hook so we will be able to see what happened when we crash.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use bevy::prelude::*;
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    
+    console_error_panic_hook::set_once();
     App::new()
         .add_plugins(DefaultPlugins)
         .run();
